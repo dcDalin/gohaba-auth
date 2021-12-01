f356c6d8f4692e1d212064ad102261261d67b478
dcDalin <mcdalinoluoch@gmail.com>
2021-12-01 03:46:49 +0300

    Fix webhook

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,5 +1,5 @@
-import admin from 'firebase-admin';
-import type { NextApiRequest, NextApiResponse } from 'next';
+import admin from "firebase-admin";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 const privateKey = process.env.FIREBASE_PRIVATE_KEY
   ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
@@ -19,39 +19,43 @@ const extractToken = (bearerToken) => {
 // Initialize the Firebase admin SDK with your service account credentials
 if (privateKey) {
   try {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-        privateKey
-      })
-    });
+    if (admin.apps.length === 0) {
+      admin.initializeApp({
+        credential: admin.credential.cert({
+          projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+          clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+          privateKey,
+        }),
+      });
+    }
   } catch (err) {
     error = err;
-    console.error('Something went wrong: ', err);
+    console.error("Something went wrong: ", err);
   }
 }
 
 const webhook = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
+    console.log("One is fired");
     // Throw 500 if firebase is not configured
     if (!privateKey) {
-      res.status(500).send('Firebase not configured');
+      res.status(500).send("Firebase not configured");
       return;
     }
 
     // Check for errors initializing firebase SDK
     if (error) {
-      res.status(500).send('Invalid firebase configuration');
+      res.status(500).send("Invalid firebase configuration");
       return;
     }
 
     // Get authorization headers
-    const authHeaders = req.headers['Authorization'];
+    const authHeaders = req.headers["Authorization"];
 
+    console.log("************** webhook");
     // Send anonymous role if there are no auth headers
     if (!authHeaders) {
-      res.json({ 'x-hasura-role': 'anonymous' });
+      res.json({ "x-hasura-role": "anonymous" });
       return;
     } else {
       // Validate the received id_token
@@ -62,8 +66,8 @@ const webhook = async (req: NextApiRequest, res: NextApiResponse) => {
         .verifyIdToken(idToken)
         .then((decodedToken) => {
           const hasuraVariables = {
-            'X-Hasura-User-Id': decodedToken.uid,
-            'X-Hasura-Role': 'user'
+            "X-Hasura-User-Id": decodedToken.uid,
+            "X-Hasura-Role": "user",
           };
           console.log(hasuraVariables); // For debug
           // Send appropriate variables
@@ -72,12 +76,12 @@ const webhook = async (req: NextApiRequest, res: NextApiResponse) => {
         .catch((e) => {
           // Throw authentication error
           console.log(e);
-          res.json({ 'x-hasura-role': 'anonymous' });
+          res.json({ "x-hasura-role": "anonymous" });
         });
     }
   } catch (e) {
-    console.log('Error is: *********************: ', e);
-    return res.status(500).json({ error: 'Unexpected error.' });
+    console.log("Error is: *********************: ", e);
+    return res.status(500).json({ error: "Unexpected error." });
   }
   return res.status(200).json({ success: true });
 };
